4ca5538e1690daa0169ad9dba1919083c29c7647
deepakoraon19 <deepak.19ranchi@gmail.com>
2022-05-25 08:56:16 +0530

    cart-float-box

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 let plusBtn = document.querySelector(".plus")
 let minusBtn = document.querySelector(".minus")
 let counterTxt  = document.querySelector(".count")
+let cartBtn = document.querySelector(".cart-img")
 
 let counterBtn = ()=>{
     let count=0;
